6dbaddd3e55d212c3379ac38c0996ec53fb67e58
obisin <90648621+obisin@users.noreply.github.com>
2025-08-20 18:27:15 +0100

    Update __init__.py
    added cleanup node

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
 from .dynamic_swapping_loader import DynamicSwappingLoader
 from .dgls_model_loader import DGLSModelLoader
+from. dgls_cleanup import DGLSCleanup
 
 NODE_CLASS_MAPPINGS = {
     "DGLSModelLoader": DGLSModelLoader,
-    "DynamicSwappingLoader": DynamicSwappingLoader
+    "DynamicSwappingLoader": DynamicSwappingLoader,
+    "DGLSCleanup": DGLSCleanup
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DGLSModelLoader": "DGLS Model Loader",
-    "DynamicSwappingLoader": "DGLS Swapping Loader"
+    "DynamicSwappingLoader": "DGLS Swapping Loader",
+    "DGLSCleanup": "DGLS Cleanup"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
